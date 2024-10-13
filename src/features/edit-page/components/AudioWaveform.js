847e91474d53b2e745e21e3129b124ab230c11b4
Artem Haharin <gagarin.artem.2008@gmail.com>
2024-10-14 02:14:41 +0300

    Refactor wavesurfer to use React hook

--- src/features/edit-page/components/AudioWaveform.js
+++ src/features/edit-page/components/AudioWaveform.js
@@ -1,50 +1,34 @@
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useMemo, useRef } from 'react';
+import { useWavesurfer } from '@wavesurfer/react';
 import { FileContext } from 'src/common/contexts/fileContext';
-import wavesurfer from 'wavesurfer.js';
-import TimelinePlugin from 'wavesurfer.js/dist/plugins/timeline.esm.js';
+import Timeline from 'wavesurfer.js/dist/plugins/timeline.esm.js';
 
 const AudioWaveform = () => {
-  const wavesurferRef = useRef(null);
-
+  const containerRef = useRef(null);
   const { fileURL } = useContext(FileContext);
 
-  const [wsf, setWsf] = useState(null);
+  const { wavesurfer } = useWavesurfer({
+    container: containerRef,
+    autoCenter: true,
+    cursorColor: 'violet',
+    waveColor: '#211027',
+    progressColor: '#69207F',
+    url: fileURL,
+    // @ts-ignore
+    plugins: useMemo(() => [Timeline.create()], []),
+  });
 
   useEffect(() => {
-    if (wavesurferRef.current && !wsf) {
-      setWsf(
-        wavesurfer.create({
-          container: '#waveform',
-          autoCenter: true,
-          cursorColor: 'violet',
-          waveColor: '#211027',
-          progressColor: '#69207F',
-          // @ts-ignore
-          plugins: [TimelinePlugin.create()],
-        }),
-      );
+    if (wavesurfer) {
+      // wsf.on('ready', () => {
+      //   wsf.play();
+      // });
     }
-  }, [wavesurferRef, wsf, fileURL]);
-
-  useEffect(() => {
-    if (fileURL && wsf) {
-      wsf.load(fileURL);
-    }
-  }, [fileURL, wsf]);
-
-  useEffect(() => {
-    if (wsf) {
-      if (wsf) {
-        wsf.on('ready', () => {
-          wsf.play();
-        });
-      }
-    }
-  }, [wsf]);
+  }, [wavesurfer]);
 
   return (
     <section className="waveform-container">
-      <div ref={wavesurferRef} id="waveform" />
+      <div ref={containerRef} />
     </section>
   );
 };
