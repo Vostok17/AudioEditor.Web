53aa0dca30d6217a5d794a209e947f54b5010035
Artem Haharin <gagarin.artem.2008@gmail.com>
2024-10-13 10:50:57 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import EditPage from './features/edit-page/EditPage';
+import HomePage from './features/home-page/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/edit" element={<EditPage />} />
+      </Routes>
+    </Router>
   );
 }
 
